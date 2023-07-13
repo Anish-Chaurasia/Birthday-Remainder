31cfa15e810efe3540ed41a938ca774bcf1d7f71
Anish Chaurasia <anishraut2111@gmail.com>
2023-07-13 19:29:43 +0530

    birthday remainder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    const [book, setBook] = useState([{ name: "Today is Anish Birthday", photo: "./images/anish.jpg", age: 21 },
+        { name: "Today is Sahil Birthday", photo: "./images/sahil.jpg", age: 21 }, { name: "Today is Ashish Birthday", photo: "./images/ashish.jpg", age: 22 }, { name: "Today is Amit Birthday", photo: "./images/amit.jpg", age: 26 },]);
+    let count = book.length;
+
+    return (
+
+        <>
+            < div className="container">
+
+                <p>| Total {count} Birthday is Today |</p>
+                {
+                    book.map((item) => {
+
+
+                        return (
+
+
+                            <div className="item">
+
+                                <div className="itemElement">
+
+                                    <img src={item.photo} />
+                                    <p>{item.name}</p>
+                                    <p>{item.age}</p>
+                                </div>
+                            </div>)
+
+                    })
+                }
+
+
+            </div>
+            <button type="button" className="btn" onClick={() => { setBook([]) }}><p>clear all</p></button>
+        </>
+
+    )
+
+            }
 export default App;
+
